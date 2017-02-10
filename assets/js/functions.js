c5caf84f3ae384da0a2897aec0e7fafd02485248
Taylor Milliman <twm013@bucknell.edu>
2017-02-10 10:54:03 -0500

    paralax effect

---
+++ assets/js/functions.js
@@ -0,0 +1,9 @@
+$(window).scroll(function(){
+  youtubeVidScroll();
+});
+
+function youtubeVidScroll(){
+  var wScroll = $(window).scrollTop();
+
+  $('.video-strip').css('background-position', 'center -' + wScroll + 'px');
+}
